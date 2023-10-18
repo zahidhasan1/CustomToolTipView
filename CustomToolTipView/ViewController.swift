bbb8df0f5bb9d300c51b7dcbdf6e2c6a6c6e80cb
Zahid Hasan <megamind.d.evilll@gmail.com>
2023-10-18 17:31:50 +0600

    Initial Commit

---
+++ CustomToolTipView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomToolTipView
+//
+//  Created by ZEUS on 18/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
